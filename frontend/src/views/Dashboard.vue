e6ab4d3856662e823b3a216e8133e94f7b7ec9c7
SCOTTI PJL <scottiguilherme567@gmail.com>
2024-07-11 23:12:35 -0300

    reiniciando de outra forma

---
+++ frontend/src/views/Dashboard.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+    <h1>ajwbdkawdaodn k fsjenkjsenjsnefkjsnkjefjsnhjsabjhdabjha </h1>
+    <backEffect />
+</div>
+</template>
+
+<script>
+import backEffect from '../components/backEffect.vue'
+export default {
+    components: {
+        backEffect
+    }
+}
+</script>
